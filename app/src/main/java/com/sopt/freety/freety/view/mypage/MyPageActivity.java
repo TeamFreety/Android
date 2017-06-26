bcccfa5811155c59fcf33cb2c1af70f6efd9b123
JunhoeKim <jooon.hoe94@gmail.com>
2017-06-26 15:21:41 +0900

    on going2

--- app/src/main/java/com/sopt/freety/freety/view/mypage/MyPageActivity.java
+++ app/src/main/java/com/sopt/freety/freety/view/mypage/MyPageActivity.java
@@ -1,18 +1,30 @@
 package com.sopt.freety.freety.view.mypage;
 
-import android.app.ActionBar;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.sopt.freety.freety.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
+
 public class MyPageActivity extends AppCompatActivity {
+
+    @BindView(R.id.my_page_profile)
+    ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+        ButterKnife.bind(this);
+
+        Glide.with(this).load(R.drawable.chat_list_elem)
+                .bitmapTransform(new CropCircleTransformation(getApplicationContext()))
+                .into(profileImage);
 
     }
 }
