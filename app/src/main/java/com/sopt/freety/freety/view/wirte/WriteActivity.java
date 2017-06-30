96941a79355ca969247dbedd8ec48651899eaa54
netking77 <netking77@naver.com>
2017-06-30 20:36:50 +0900

    ongoing write

---
+++ app/src/main/java/com/sopt/freety/freety/view/wirte/WriteActivity.java
@@ -0,0 +1,15 @@
+package com.sopt.freety.freety.view.wirte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sopt.freety.freety.R;
+
+public class WriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+    }
+}
