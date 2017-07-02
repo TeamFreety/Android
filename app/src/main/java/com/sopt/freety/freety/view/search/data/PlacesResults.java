9172530e03a063566e435a05c5e8fcad255c98db
JunhoeKim <jooon.hoe94@gmail.com>
2017-07-02 16:58:17 +0900

    on going

--- app/src/main/java/com/sopt/freety/freety/view/search/data/PlacesResults.java
+++ app/src/main/java/com/sopt/freety/freety/view/search/data/PlacesResults.java
@@ -1,4 +1,4 @@
-package com.sopt.freety.freety.view.recruit.data;
+package com.sopt.freety.freety.view.search.data;
 
 import java.util.List;
 
--- app/src/main/java/com/sopt/freety/freety/view/wirte/WriteActivity.java
+++ app/src/main/java/com/sopt/freety/freety/view/wirte/WriteActivity.java
@@ -1,51 +1,75 @@
 package com.sopt.freety.freety.view.wirte;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.sopt.freety.freety.R;
 import com.sopt.freety.freety.view.property.ScreenClickable;
+import com.sopt.freety.freety.view.recruit.MapPopupActivity;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class WriteActivity extends AppCompatActivity implements ScreenClickable {
 
-    @BindView(R.id.btn_wirte_cancel)
-    Button writeCancelBtn;
+    public static final int MAP_POPUP_CODE = 1234;
 
-    @BindView(R.id.checkbtn_write_sort_perm)
-    CheckBox writeSortPermCheckbtn;
-    @BindView(R.id.checkbtn_write_sort_dye)
-    CheckBox writeSortDyeCheckbtn;
-    @BindView(R.id.checkbtn_write_sort_cut)
-    CheckBox writeSortCutCheckbtn;
-    @BindView(R.id.checkbtn_write_sort_etc)
-    CheckBox writeSortEtcCheckbtn;
+    @OnClick({R.id.write_type_btn1, R.id.write_type_btn2, R.id.write_type_btn3, R.id.write_type_btn4})
+    public void onHairTypeClick(Button button) {
+        String hairTypeString = button.getText().toString();
+        if (hairTypeSet.contains(hairTypeString)) {
+            hairTypeSet.remove(hairTypeString);
+            button.setTextColor(Color.parseColor("#95989A"));
+            button.setBackgroundResource(R.drawable.hair_type_btn_unselected);
+        } else {
+            hairTypeSet.add(hairTypeString);
+            button.setTextColor(Color.WHITE);
+            button.setBackgroundResource(R.drawable.hair_type_btn_selected);
+        }
+    }
 
-    @BindView(R.id.spinner_write_month)
-    Spinner writeMonthSpinner;
-    @BindView(R.id.spinner_write_day)
-    Spinner writeDaySpinner;
-    @BindView(R.id.spinner_write_time)
-    Spinner writeTimeSpinner;
+    @BindView(R.id.write_date_btn)
+    ImageButton writeDateBtn;
 
-    @BindView(R.id.btn_write_search_map)
-    Button writeSearchMapBtn;
-    @BindView(R.id.text_write_location_result)
-    TextView writeLocationResultText;
+    @OnClick(R.id.write_date_btn)
+    public void onDateBtn() {
+        new DatePickerDialog(WriteActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
 
-    @BindView(R.id.btn_write_image_add)
-    Button writeImageAddBtn;
+    @BindView(R.id.write_date_text)
+    TextView writeDateText;
+
+    @OnClick(R.id.write_map_search_btn)
+    public void onMapSearchBtn() {
+        startActivityForResult(new Intent(WriteActivity.this, MapPopupActivity.class), MAP_POPUP_CODE);
+    }
+
+    @BindViews({R.id.write_map_text1, R.id.write_map_text2})
+    List<TextView> mapTextList;
+
     @BindView(R.id.img_write_selected_first)
     ImageView writeSelectedFirstImg;
     @BindView(R.id.img_write_selected_second)
@@ -66,19 +90,20 @@ public class WriteActivity extends AppCompatActivity implements ScreenClickable 
     @BindView(R.id.btn_write_register)
     Button writeRegisterBtn;
 
+    @OnClick(R.id.btn_write_back)
+    public void onBackBtn() {
+        finish();
+    }
+
+    private Set<String> hairTypeSet = new HashSet<>();
+    private GregorianCalendar calendar = new GregorianCalendar();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_write);
         ButterKnife.bind(this);
-        writeCancelBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
     }
 
     @Override
@@ -86,5 +111,34 @@ public class WriteActivity extends AppCompatActivity implements ScreenClickable 
         InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(writeTitleEdit.getWindowToken(), 0);
         imm.hideSoftInputFromWindow(writeContentEdit.getWindowToken(), 0);
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            writeDateText.setText(String.format("%d년 %d월 %d일", year, month, dayOfMonth));
+            new TimePickerDialog(WriteActivity.this, timeSetListener,
+                    calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show();
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            String dateString = writeDateText.getText().toString();
+            String viewString;
+            if (hourOfDay > 12) {
+                viewString = dateString + String.format(" 오후 %d시", hourOfDay - 12);
+            } else {
+                viewString = dateString + String.format(" 오후 %d시", hourOfDay);
+            }
+            writeDateText.setText(viewString);
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        data.getStringExtra("address");
+        mapTextList
     }
 }
