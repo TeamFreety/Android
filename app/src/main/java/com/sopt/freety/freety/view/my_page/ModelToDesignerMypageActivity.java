5de25e13ae7038e23ca7958fb29254cd64247b42
netking77 <netking77@naver.com>
2017-07-06 02:40:20 +0900

    ongoing

---
+++ app/src/main/java/com/sopt/freety/freety/view/my_page/ModelToDesignerMypageActivity.java
@@ -0,0 +1,15 @@
+package com.sopt.freety.freety.view.my_page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sopt.freety.freety.R;
+
+public class ModelToDesignerMypageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_model_to_designer_mypage);
+    }
+}
