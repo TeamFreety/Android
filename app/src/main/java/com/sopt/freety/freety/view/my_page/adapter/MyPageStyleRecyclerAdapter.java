331f59f21e3ef82fe3907dac3db084869f95da9e
JunhoeKim <jooon.hoe94@gmail.com>
2017-07-07 21:37:12 +0900

    commit

--- app/src/main/java/com/sopt/freety/freety/view/my_page/adapter/MyPageStyleRecyclerAdapter.java
+++ app/src/main/java/com/sopt/freety/freety/view/my_page/adapter/MyPageStyleRecyclerAdapter.java
@@ -55,7 +55,6 @@ public class MyPageStyleRecyclerAdapter extends RecyclerView.Adapter<RecyclerVie
         if (holder instanceof MyPageStyleHeaderHolder) {
             MyPageStyleHeaderHolder castedHolder = (MyPageStyleHeaderHolder) holder;
             castedHolder.getCareerText().setText(careerString);
-            //TODO: implement pop up to edit text
         } else {
             MyPageStyleBodyHolder castedHolder = (MyPageStyleBodyHolder) holder;
             Glide.with(context).load(R.drawable.chat_list_elem).override(200, 200).centerCrop().fitCenter().into(castedHolder.getStyleImage());
